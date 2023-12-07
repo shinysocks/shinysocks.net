b741296357ecfb46087aa403a52c80397e2ef45f
shinysocks <noah@shinysocks.net>
2023-12-07 17:32:05 -0600

    remove old pieces and simplify site

--- src/main.rs
+++ src/main.rs
@@ -9,29 +9,9 @@ async fn home() -> Option<NamedFile> {
     NamedFile::open("static/index.html").await.ok()
 }
 
-#[get("/robots.txt")]
-async fn robots() -> Option<NamedFile> {
-    NamedFile::open("static/robots.txt").await.ok()
-}
-
-#[get("/keanukalc")]
-async fn keanukalc() -> Option<NamedFile> {
-    NamedFile::open("static/keanukalc.html").await.ok()
-}
-
-#[get("/play")]
-async fn play() -> Option<NamedFile> {
-    NamedFile::open("static/play.html").await.ok()
-}
-
 #[get("/radio")]
 async fn radio() -> Option<NamedFile> {
     NamedFile::open(randomfile("./tunes")).await.ok()
-}
-
-#[get("/bluey480gfyrugky7e4f606")]
-async fn bluey() -> Option<NamedFile> {
-    NamedFile::open(randomfile("./share/bluey480gfyrugky7e4f606")).await.ok()
 }
 
 #[catch(404)]
@@ -43,7 +23,7 @@ async fn not_found() -> Option<NamedFile> {
 fn rocket() -> _ {
     rocket::build()
     .register("/", catchers![not_found])
-    .mount("/", routes![home, keanukalc, play, radio, bluey, robots])
+    .mount("/", routes![home, radio])
     .mount("/", FileServer::from("static/"))
     .mount("/share", FileServer::from("share/").rank(2))
 }
