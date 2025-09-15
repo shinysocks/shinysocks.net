f07babd1784be8bd926ef9d6a23291cf68efb3e4
Noah Dinan <noah@shinysocks.net>
2025-09-14 23:28:40 -0500

    completed shitty stuff

--- src/routes.h
+++ src/routes.h
@@ -1,68 +1,40 @@
+#include <filesystem>
 #include <httplib.h>
 
 using namespace httplib;
 using namespace std;
+using namespace filesystem;
 
-const string HTML_RES = R"(
-    <!DOCTYPE html>
-    <head>
-    <title>Noah Dinan</title>
-    </head>
-    <style>
-    @import url(https://fonts.googleapis.com/css2?family=Fira+Code);
-    * { font-family: Fira Code;background-color:black;font-weight:bolder;display:inline; }
-    img { width:310px;-moz-user-select:none;-webkit-user-select:none;user-select:none; }
-    div { position:absolute;top:50%;left:50%;transform:translate(-50%,-50%);text-align:center; }
-    p { color:#9399b2; }
-    a { color:#b4befe; }
-    audio { display:none; }
-    </style>
-    <body>
-      <div>
-        <img src="logo.png" />
-        <br/>
-        <br/>
-        <a href="https://github.com/shinysocks">github.com/shinysocks</a>
-        <br/><br/>
-        <p style="color:#a6e3a1;-moz-user-select:none;-webkit-user-select:none;user-select:none;">$ </p>
-        <p>curl shinysocks.net</p>
-        <audio autoplay="true" src="http://localhost:8888/radio"/>
-      </div>
-    </body>
-    </html>
-)";
+string current_song;
+int current_time;
+
+const string TUNES_PATH = "/home/shinysocks/sync/tunes/";
 
 class Routes {
     public:
 
-    constexpr static auto root = [](const auto &req, auto &res) {
-        string user_agent;
+    constexpr static auto root = [](const auto &req, Response &res) {
+        string user_agent = "";
         if (req.has_header("User-Agent")) {
             user_agent = req.get_header_value("User-Agent");
-            cout << user_agent << endl;
-            res.set_header("Secret-Agent", user_agent);
-        } else {
-            // bot found!
         }
 
         if (user_agent.find("curl") != std::string::npos) {
             res.set_file_content("./share/index");
         } else if (user_agent.length() == 0) {
-            res.status = 418;
-            res.set_content("hello bot", "text/html");
+            res.set_redirect("http://localhost:8888/bomb");
         } else {
-            res.set_content(HTML_RES, "text/html");
+            res.set_file_content("./share/home.html", "text/html");
         }
     };
 
     constexpr static auto favicon = [](const auto &, auto &res) {
-        // res.status = 200; <- do I need this?
         res.set_content("", "image/jpeg");
     };
 
     constexpr static auto bomb = [](const auto &req, auto &res) {
         // Inspired by https://idiallo.com/blog/zipbomb-protection
-        res.set_file_content("./share/bomb.html", "text/html");
+        res.set_file_content("./share/bomb", "text/html");
         res.set_header("Content-Encoding", "gzip");
         res.set_header("Content-Length", "1000");
     };
@@ -72,18 +44,14 @@ class Routes {
     };
 
     constexpr static auto radio = [](const auto &req, auto &res) {
-        res.set_file_content("./share/indigo.mp3", "audio/mp3");
-
-        // res.set_chunked_content_provider(
-        //     "audio/mpeg",
-        //     [](size_t offset, DataSink &sink) {
-        //         for (int i = 0; i < 10000000; i++) {
-        //             sink.write("hi", 2);
-        //         } sink.done();
-        //         return true;
-        //     }
-        // );
+        res.set_file_content(current_song, "audio/mp3");
+        res.set_header("Song", current_song);
     };
+
+    constexpr static auto radio_elapsed = [](const auto &req, auto &res) {
+        res.set_content(to_string(current_time), "text/plain");
+    };
+
 };
 
 
--- src/server.cpp
+++ src/server.cpp
@@ -1,4 +1,5 @@
-#include <httplib.h>
+// #include <httplib.h>
+#include "httplib.h"
 #include "routes.h"
 
 using namespace httplib;
@@ -7,23 +8,60 @@ int PORT = 8888;
 string ADDR = "localhost";
 
 int main(void) {
+
+    // load music files
+    vector<string> filenames;
+
+    for (const auto &f : directory_iterator(TUNES_PATH)) {
+        filenames.push_back(f.path());
+    }
+
+    cout << "found " << filenames.size() << " songs in " << TUNES_PATH << endl;
+
+    auto audio = thread([&](){
+        while (true) {
+            current_time = 0;
+            srand(time(nullptr));
+            int i = rand() % filenames.size();
+            current_song = filenames.at(i);
+            int first = current_song.find_last_of("/") + 1;
+            int last = current_song.find_first_of(".");
+            
+            int duration = stoi(current_song.substr(first, last));
+
+            cout << "playing: " << current_song << " for " << duration << " seconds" << endl;
+
+            for (int i = 0; i < duration+2; i++) {
+                this_thread::sleep_for(1000ms);
+                current_time++;
+            }
+        }
+    });
+
+
     Server svr;
 
     // routes
     svr.Get("/", Routes::root);
     svr.Get("/favicon.ico", Routes::favicon);
-    svr.Get("/bomb.html", Routes::bomb);
+    svr.Get("/bomb", Routes::bomb);
     svr.Get("/pong", Routes::pong);
     svr.Get("/radio", Routes::radio);
+    svr.Get("/radio/elapsed", Routes::radio_elapsed);
 
     svr.set_base_dir("./share");
 
-    svr.set_error_handler([](const Request &, Response &res) {
+    svr.set_logger([](const Request& req, const auto& res) {
+        string user_agent = "";
+        if (req.has_header("User-Agent")) {
+            user_agent = req.get_header_value("User-Agent");
+        }
 
-    });
+        if (user_agent.length() == 0) {
+            user_agent = "bot";
+        }
 
-    svr.set_logger([](const auto& req, const auto& res) {
-
+         cout << req.remote_addr << ": " << req.path << " [" << user_agent << "]" << endl;
     });
 
     svr.listen(ADDR, PORT);
